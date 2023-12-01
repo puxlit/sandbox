d1a7d333219017f3a3b3487689a7c9aedbf3c67a
Xiao Di Guan <xdg@puxlit.net>
2023-12-02 07:29:49 +1100

    Refactor in preparation for second part of day 1
    [🫥 Rewritten from commit b9ee83614af4a434eca1e5aca3ee21ff1d92e09b.]

--- 1/trebuchet.py
+++ 1/trebuchet.py
@@ -16,18 +16,18 @@ ASCII_ZERO = 0x30
 ASCII_NINE = 0x39
 
 
-def extract_first_digit(chars: Iterable[int]) -> int:
+def extract_naive_first_digit(chars: Iterable[int]) -> int:
     for char in chars:
         if ASCII_ZERO <= char <= ASCII_NINE:
             return char - ASCII_ZERO
     raise ValueError()
 
 
-def extract_calibration_values(lines: Iterable[bytes]) -> Iterator[int]:
+def extract_naive_calibration_values(lines: Iterable[bytes]) -> Iterator[int]:
     """
     Extract the calibration value (comprising the first and last digits) from each line.
 
-    >>> list(extract_calibration_values([
+    >>> list(extract_naive_calibration_values([
     ...     b'1abc2',
     ...     b'pqr3stu8vwx',
     ...     b'a1b2c3d4e5f',
@@ -36,13 +36,13 @@ def extract_calibration_values(lines: Iterable[bytes]) -> Iterator[int]:
     [12, 38, 15, 77]
     """
     for line in lines:
-        first_digit = extract_first_digit(line)
-        last_digit = extract_first_digit(reversed(line))
+        first_digit = extract_naive_first_digit(line)
+        last_digit = extract_naive_first_digit(reversed(line))
         yield (first_digit * 10) + last_digit
 
 
-def sum_calibration_values(lines: Iterable[bytes]) -> int:
-    return sum(extract_calibration_values(lines))
+def sum_naive_calibration_values(lines: Iterable[bytes]) -> int:
+    return sum(extract_naive_calibration_values(lines))
 
 
 ########################################################################################################################
@@ -53,10 +53,14 @@ def main() -> None:
     import argparse
 
     parser = argparse.ArgumentParser()
+    parser.add_argument('part', type=int, choices=(1,))
     parser.add_argument('input', type=argparse.FileType('rb'))
     args = parser.parse_args()
 
-    print(sum_calibration_values(args.input))
+    if (args.part == 1):
+        print(sum_naive_calibration_values(args.input))
+    else:
+        raise ValueError()
 
 
 if __name__ == '__main__':
