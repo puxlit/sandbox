dafd0275694ca003d0481b3054843f1333ffe3c8
Xiao Di Guan <xdg@puxlit.net>
2023-12-04 20:13:39 +1100

    Solve second part of day 4

--- 4/scratchcards.py
+++ 4/scratchcards.py
@@ -111,6 +111,44 @@ def sum_points(lines: Iterable[str]) -> int:
 
 
 ########################################################################################################################
+# Part 2
+########################################################################################################################
+
+def count_total_cards(lines: Iterable[str]) -> int:
+    """
+    >>> count_total_cards([
+    ...     'Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53',
+    ...     'Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19',
+    ...     'Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1',
+    ...     'Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83',
+    ...     'Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36',
+    ...     'Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11',
+    ... ])
+    30
+    >>> count_total_cards([
+    ...     'Card 1: 1 | 1',
+    ... ])
+    Traceback (most recent call last):
+        ...
+    ValueError: Ran out of cards to copy when processing Card(id_=1, winning_numbers=(1,), numbers=(1,), matching_numbers=(1,), points=1); needed 1 more card(s)
+    """
+    cards = list(Card.from_lines(lines))
+    total_original_cards = len(cards)
+    copied_card_counts = [0] * total_original_cards
+    for (i, card) in enumerate(cards):
+        num_matching_numbers = len(card.matching_numbers)
+        num_missing_cards = (i + num_matching_numbers) - (total_original_cards - 1)
+        if num_missing_cards > 0:
+            raise ValueError(f'Ran out of cards to copy when processing {card}; '
+                             f'needed {num_missing_cards} more card(s)')
+        card_count = 1 + copied_card_counts[i]
+        for j in range(i + 1, i + 1 + num_matching_numbers):
+            copied_card_counts[j] += card_count
+    total_copied_cards = sum(copied_card_counts)
+    return total_original_cards + total_copied_cards
+
+
+########################################################################################################################
 # CLI bootstrap
 ########################################################################################################################
 
@@ -125,6 +163,8 @@ def main() -> None:
 
     if args.part == 1:
         print(sum_points(lines))
+    elif args.part == 2:
+        print(count_total_cards(lines))
     else:
         raise ValueError(f'{args.part} is not a valid part')
 
