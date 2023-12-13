7a01b15f4f6ccf583ac54d4532fa8a36e4267f91
Xiao Di Guan <xdg@puxlit.net>
2023-12-14 00:07:47 +1100

    Solve second part of day 13
    [🫥 Rewritten from commit 5416d438116620c5e6930c5f6e14797294236ae6.]

--- 13/point_of_incidence.py
+++ 13/point_of_incidence.py
@@ -6,13 +6,28 @@
 ########################################################################################################################
 
 from collections.abc import Iterator
-from itertools import chain
+from itertools import chain, combinations
 from typing import NamedTuple
 
 
 ########################################################################################################################
-# Part 1
+# Notes
 ########################################################################################################################
+
+def single_bit_diff_position(a: int, b: int) -> int:
+    x = a ^ b
+    if x == 0:
+        # Inputs are identical.
+        return -1
+    if x & (x - 1) != 0:
+        # XORed inputs is not a power of two; multiple bits are set.
+        return -1
+    i = -1
+    while x:
+        x >>= 1
+        i += 1
+    return i
+
 
 ASH = '.'
 ROCKS = '#'
@@ -54,7 +69,7 @@ class Pattern(NamedTuple):
             rows.append(row)
         return Pattern(width, height, tuple(columns), tuple(rows))
 
-    def summarise(self) -> int:
+    def summarise(self, ignore_summary: int = 0) -> int:
         """
         >>> Pattern.from_lines(iter([
         ...     '#.##..##.',
@@ -108,8 +123,10 @@ class Pattern(NamedTuple):
                 continue
             if tuple(reversed(self.columns[x + 1 - reflection_length:x + 1])) != self.columns[x + 1:x + 1 + reflection_length]:
                 continue
-            best_reflection_length = reflection_length
-            best_summary = COLUMN_REFLECTION_SUMMARY_FACTOR * (x + 1)
+            summary = COLUMN_REFLECTION_SUMMARY_FACTOR * (x + 1)
+            if summary != ignore_summary:
+                best_reflection_length = reflection_length
+                best_summary = summary
         for y in range(0, self.height - 1):
             if self.rows[y] != self.rows[y + 1]:
                 continue
@@ -118,10 +135,67 @@ class Pattern(NamedTuple):
                 continue
             if tuple(reversed(self.rows[y + 1 - reflection_length:y + 1])) != self.rows[y + 1:y + 1 + reflection_length]:
                 continue
-            best_reflection_length = reflection_length
-            best_summary = ROW_REFLECTION_SUMMARY_FACTOR * (y + 1)
+            summary = ROW_REFLECTION_SUMMARY_FACTOR * (y + 1)
+            if summary != ignore_summary:
+                best_reflection_length = reflection_length
+                best_summary = summary
         return best_summary
 
+    def flip_bit(self, x: int, y: int) -> 'Pattern':
+        assert 0 <= x <= self.width
+        assert 0 <= y <= self.height
+        modified_column = self.columns[x] ^ (1 << y)
+        modified_columns = self.columns[:x] + (modified_column,) + self.columns[x + 1:]
+        modified_row = self.rows[y] ^ (1 << x)
+        modified_rows = self.rows[:y] + (modified_row,) + self.rows[y + 1:]
+        return Pattern(self.width, self.height, modified_columns, modified_rows)
+
+    def summarise_repaired(self) -> int:
+        """
+        >>> Pattern.from_lines(iter([
+        ...     '#.##..##.',
+        ...     '..#.##.#.',
+        ...     '##......#',
+        ...     '##......#',
+        ...     '..#.##.#.',
+        ...     '..##..##.',
+        ...     '#.#.##.#.',
+        ... ])).summarise_repaired()
+        300
+        >>> Pattern.from_lines(iter([
+        ...     '#...##..#',
+        ...     '#....#..#',
+        ...     '..##..###',
+        ...     '#####.##.',
+        ...     '#####.##.',
+        ...     '..##..###',
+        ...     '#....#..#',
+        ... ])).summarise_repaired()
+        100
+        """
+        smudged_summary = self.summarise()
+        for ((i_0, column_0), (i_1, column_1)) in combinations(enumerate(self.columns), 2):
+            bit_diff_position = single_bit_diff_position(column_0, column_1)
+            if bit_diff_position < 0:
+                continue
+            modified_pattern = self.flip_bit(i_0, bit_diff_position)
+            new_summary = modified_pattern.summarise(ignore_summary=smudged_summary)
+            if new_summary > 0:
+                return new_summary
+        for ((i_0, row_0), (i_1, row_1)) in combinations(enumerate(self.rows), 2):
+            bit_diff_position = single_bit_diff_position(row_0, row_1)
+            if bit_diff_position < 0:
+                continue
+            modified_pattern = self.flip_bit(bit_diff_position, i_0)
+            new_summary = modified_pattern.summarise(ignore_summary=smudged_summary)
+            if new_summary > 0:
+                return new_summary
+        return 0
+
+
+########################################################################################################################
+# Part 1
+########################################################################################################################
 
 def patterns_from_lines(lines: Iterator[str]) -> Iterator[Pattern]:
     while True:
@@ -158,6 +232,35 @@ def summarise_notes(lines: Iterator[str]) -> int:
 
 
 ########################################################################################################################
+# Part 2
+########################################################################################################################
+
+def summarise_repaired_notes(lines: Iterator[str]) -> int:
+    """
+    >>> summarise_repaired_notes(iter([
+    ...     '#.##..##.',
+    ...     '..#.##.#.',
+    ...     '##......#',
+    ...     '##......#',
+    ...     '..#.##.#.',
+    ...     '..##..##.',
+    ...     '#.#.##.#.',
+    ...     '',
+    ...     '#...##..#',
+    ...     '#....#..#',
+    ...     '..##..###',
+    ...     '#####.##.',
+    ...     '#####.##.',
+    ...     '..##..###',
+    ...     '#....#..#',
+    ... ]))
+    400
+    """
+    patterns = patterns_from_lines(lines)
+    return sum(pattern.summarise_repaired() for pattern in patterns)
+
+
+########################################################################################################################
 # CLI bootstrap
 ########################################################################################################################
 
@@ -172,6 +275,8 @@ def main() -> None:
 
     if args.part == 1:
         print(summarise_notes(lines))
+    elif args.part == 2:
+        print(summarise_repaired_notes(lines))
     else:
         raise ValueError(f'{args.part} is not a valid part')
 
