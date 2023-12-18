ca3f3459bc75d15ba4bc9dd16ac62aa7c7820ccc
Xiao Di Guan <xdg@puxlit.net>
2023-12-19 05:15:26 +1100

    Attempt first part of day 18

---
+++ 18/lavaduct_lagoon.py
@@ -0,0 +1,287 @@
+#!/usr/bin/env python3
+
+
+########################################################################################################################
+# Imports
+########################################################################################################################
+
+from collections.abc import Iterable, Iterator
+from enum import Enum
+import re
+from typing import NamedTuple
+
+
+########################################################################################################################
+# Dig plan
+########################################################################################################################
+
+def pairs(iterable: Iterable[int]) -> Iterator[tuple[int, int]]:
+    iter_ = iter(iterable)
+    while True:
+        try:
+            a = next(iter_)
+        except StopIteration:
+            return
+        try:
+            b = next(iter_)
+        except StopIteration:
+            raise ValueError(f'Cannot finish pairing off odd number of elements in iterable; {a!r} is alone')
+        yield (a, b)
+
+
+class Direction(Enum):
+    UP = 'U'
+    RIGHT = 'R'
+    DOWN = 'D'
+    LEFT = 'L'
+
+    @property
+    def reverse(self):
+        return {
+            Direction.UP: Direction.DOWN,
+            Direction.RIGHT: Direction.LEFT,
+            Direction.DOWN: Direction.UP,
+            Direction.LEFT: Direction.RIGHT,
+        }[self]
+
+    @property
+    def clockwise_orthogonal(self):
+        return {
+            Direction.UP: Direction.RIGHT,
+            Direction.RIGHT: Direction.DOWN,
+            Direction.DOWN: Direction.LEFT,
+            Direction.LEFT: Direction.UP,
+        }[self]
+
+    @property
+    def anticlockwise_orthogonal(self):
+        return {
+            Direction.UP: Direction.LEFT,
+            Direction.RIGHT: Direction.UP,
+            Direction.DOWN: Direction.RIGHT,
+            Direction.LEFT: Direction.DOWN,
+        }[self]
+
+
+class Coordinate(NamedTuple):
+    x: int
+    y: int
+
+    def translate(self, direction: Direction, length: int) -> 'Coordinate':
+        if direction == Direction.UP:
+            return Coordinate(self.x, self.y - length)
+        elif direction == Direction.RIGHT:
+            return Coordinate(self.x + length, self.y)
+        elif direction == Direction.DOWN:
+            return Coordinate(self.x, self.y + length)
+        elif direction == Direction.LEFT:
+            return Coordinate(self.x - length, self.y)
+        else:
+            raise ValueError(f'Unexpected direction: {direction!r}')
+
+
+RGB_HEXADECIMAL_COLOUR_CODE_PATTERN = re.compile(r'^#([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})$')
+
+
+class Colour(NamedTuple):
+    red: int
+    green: int
+    blue: int
+
+    @classmethod
+    def from_rgb_hexadecimal_colour_code(cls, colour_code: str) -> 'Colour':
+        match = RGB_HEXADECIMAL_COLOUR_CODE_PATTERN.fullmatch(colour_code)
+        if not match:
+            raise ValueError(f'Invalid RGB hexadecimal colour code: {colour_code!r}')
+        return Colour(*(int(component, 16) for component in match.groups()))
+
+
+DIG_INSTRUCTION_PATTERN = re.compile(r'^([DLRU]) ([1-9]\d*) \((#[0-9a-f]{6})\)$')
+
+
+class DigInstruction(NamedTuple):
+    start_grid_coord: Coordinate
+    end_grid_coord: Coordinate
+    direction: Direction
+    length: int
+    colour: Colour
+
+    @classmethod
+    def from_line(cls, line: str, start_grid_coord: Coordinate, allowed_directions: set[Direction]) -> 'DigInstruction':
+        match = DIG_INSTRUCTION_PATTERN.fullmatch(line)
+        if not match:
+            raise ValueError(f'Invalid dig instruction: {line!r}')
+        (raw_direction, raw_length, raw_colour) = match.groups()
+        direction = Direction(raw_direction)
+        if direction not in allowed_directions:
+            raise ValueError(f'Invalid dig instruction (direction not allowed): {line!r}')
+        length = int(raw_length)
+        end_grid_coord = start_grid_coord.translate(direction, length)
+        colour = Colour.from_rgb_hexadecimal_colour_code(raw_colour)
+        return DigInstruction(start_grid_coord, end_grid_coord, direction, length, colour)
+
+
+class DigPlan(NamedTuple):
+    max_grid_coord: Coordinate
+    instructions: tuple[DigInstruction, ...]
+    span_grid_coords_by_row: tuple[tuple[tuple[int, int], ...], ...]
+
+    @classmethod
+    def from_lines(cls, lines: Iterable[str]) -> 'DigPlan':
+        instructions: list[DigInstruction] = []
+        origin_grid_coord = Coordinate(0, 0)
+        min_grid_coord = origin_grid_coord
+        max_grid_coord = origin_grid_coord
+        start_grid_coord = origin_grid_coord
+        allowed_directions = {Direction.UP, Direction.RIGHT, Direction.DOWN, Direction.LEFT}
+        for line in lines:
+            instruction = DigInstruction.from_line(line, start_grid_coord, allowed_directions)
+
+            if instruction.end_grid_coord.x < min_grid_coord.x:
+                min_grid_coord = Coordinate(instruction.end_grid_coord.x, min_grid_coord.y)
+            if instruction.end_grid_coord.y < min_grid_coord.y:
+                min_grid_coord = Coordinate(min_grid_coord.x, instruction.end_grid_coord.y)
+            if instruction.end_grid_coord.x > max_grid_coord.x:
+                max_grid_coord = Coordinate(instruction.end_grid_coord.x, max_grid_coord.y)
+            if instruction.end_grid_coord.y > max_grid_coord.y:
+                max_grid_coord = Coordinate(max_grid_coord.x, instruction.end_grid_coord.y)
+
+            instructions.append(instruction)
+            start_grid_coord = instruction.end_grid_coord
+            allowed_directions = {instruction.direction.clockwise_orthogonal, instruction.direction.anticlockwise_orthogonal}
+        if instruction.end_grid_coord != origin_grid_coord:
+            raise ValueError(f'Sequence of instructions do not return to the origin: {instructions!r}')
+        if instructions[0].direction not in allowed_directions:
+            # This guarantees that all instruction start/end coordinates are vertices on the formed polygon (and _don't_
+            # lie on a straight line)/
+            raise ValueError(f'Sequence of instructions do not return to the origin in a permissible manner: {instructions!r}')
+
+        translated_instructions = tuple(DigInstruction(
+            Coordinate(instruction.start_grid_coord.x - min_grid_coord.x, instruction.start_grid_coord.y - min_grid_coord.y),
+            Coordinate(instruction.end_grid_coord.x - min_grid_coord.x, instruction.end_grid_coord.y - min_grid_coord.y),
+            instruction.direction,
+            instruction.length,
+            instruction.colour,
+        ) for instruction in instructions)
+        translated_max_grid_coord = Coordinate(max_grid_coord.x - min_grid_coord.x, max_grid_coord.y - min_grid_coord.y)
+
+        corner_grid_coords_by_row: list[set[int]] = [set() for _ in range(translated_max_grid_coord.y + 1)]
+        for translated_instruction in translated_instructions:
+            corner_grid_coords_by_row[translated_instruction.end_grid_coord.y].add(translated_instruction.end_grid_coord.x)
+        span_grid_coords_by_row = tuple(tuple(span for span in pairs(sorted(row_corner_grid_coords))) for row_corner_grid_coords in corner_grid_coords_by_row)
+
+        return DigPlan(translated_max_grid_coord, translated_instructions, span_grid_coords_by_row)
+
+    def calculate_volume(self) -> int:
+        volume = 0
+        mask_spans: list[tuple[int, int]] = []
+        for row_spans in self.span_grid_coords_by_row:
+            should_skip_mask_update = False
+            if not mask_spans:
+                mask_spans = list(row_spans)
+                should_skip_mask_update = True
+            for (mask_span_start, mask_span_end) in mask_spans:
+                volume += mask_span_end - mask_span_start + 1
+            if should_skip_mask_update:
+                continue
+            for row_span in row_spans:
+                (row_span_start, row_span_end) = row_span
+                for (i, mask_span) in enumerate(mask_spans):
+                    (mask_span_start, mask_span_end) = mask_span
+                    if row_span_end < mask_span_start:
+                        # Row span is disjoint, strictly to the left of the mask span. Add to mask spans.
+                        mask_spans.insert(i, row_span)
+                        volume += row_span_end - row_span_start + 1
+                        break
+                    elif row_span_end == mask_span_start:
+                        # Row span's right side touches mask span's left side. Extend mask span.
+                        mask_spans[i] = (row_span_start, mask_span_end)
+                        volume += row_span_end - row_span_start
+                        break
+                    elif row_span == mask_span:
+                        # Row span and mask span are identical. Delete mask span.
+                        mask_spans.pop(i)
+                        break
+                    elif mask_span_start <= row_span_start and row_span_end <= mask_span_end:
+                        # Row span is covered by mask span. Subtract mask span.
+                        mask_spans.pop(i)
+                        if row_span_end != mask_span_end:
+                            mask_spans.insert(i, (row_span_end, mask_span_end))
+                        if mask_span_start != row_span_start:
+                            mask_spans.insert(i, (mask_span_start, row_span_start))
+                        break
+                    elif mask_span_end == row_span_start:
+                        # Row span's left side touches mask span's right side. Extend mask span.
+                        mask_spans[i] = (mask_span_start, row_span_end)
+                        volume += row_span_end - row_span_start
+                        break
+                    elif i == len(mask_spans) - 1:
+                        # We've exhausted all mask spans. Row span must be disjoint, strictly to the right of the mask
+                        # span. Add to mask spans.
+                        mask_spans.append(row_span)
+                        volume += row_span_end - row_span_start + 1
+                        break
+                # Compaction pass.
+                i = 0
+                while i < (len(mask_spans) - 1):
+                    (mask_span_start, mask_span_end) = mask_spans[i]
+                    (next_mask_span_start, next_mask_span_end) = mask_spans[i + 1]
+                    if next_mask_span_start == mask_span_end:
+                        mask_spans.pop(i)
+                        mask_spans.pop(i)
+                        mask_spans.insert(i, (mask_span_start, next_mask_span_end))
+                    i += 1
+        # We should wind up with an empty mask at the very end.
+        assert not mask_spans
+        return volume
+
+
+########################################################################################################################
+# Part 1
+########################################################################################################################
+
+def calculate_volume(lines: Iterable[str]) -> int:
+    """
+    >>> calculate_volume([
+    ...     'R 6 (#70c710)',
+    ...     'D 5 (#0dc571)',
+    ...     'L 2 (#5713f0)',
+    ...     'D 2 (#d2c081)',
+    ...     'R 2 (#59c680)',
+    ...     'D 2 (#411b91)',
+    ...     'L 5 (#8ceee2)',
+    ...     'U 2 (#caa173)',
+    ...     'L 1 (#1b58a2)',
+    ...     'U 2 (#caa171)',
+    ...     'R 2 (#7807d2)',
+    ...     'U 3 (#a77fa3)',
+    ...     'L 2 (#015232)',
+    ...     'U 2 (#7a21e3)',
+    ... ])
+    62
+    """
+    dig_plan = DigPlan.from_lines(lines)
+    return dig_plan.calculate_volume()
+
+
+########################################################################################################################
+# CLI bootstrap
+########################################################################################################################
+
+def main() -> None:
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('part', type=int, choices=(1, 2))
+    parser.add_argument('input', type=argparse.FileType('rt'))
+    args = parser.parse_args()
+    lines = (line.rstrip('\n') for line in args.input)
+
+    if args.part == 1:
+        print(calculate_volume(lines))
+    else:
+        raise ValueError(f'{args.part} is not a valid part')
+
+
+if __name__ == '__main__':
+    main()
