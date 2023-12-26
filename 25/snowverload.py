d68aef01819ab3d4023a245215bcc23d79af0aa1
Xiao Di Guan <xdg@puxlit.net>
2023-12-27 06:02:01 +1100

    Improve performance for first part of day 25

--- 25/snowverload.py
+++ 25/snowverload.py
@@ -7,9 +7,10 @@
 
 from collections import Counter
 from collections.abc import Iterable
+from math import ceil, comb, log
 from random import randrange
 import re
-from typing import NamedTuple
+from typing import NamedTuple, Optional
 
 
 ########################################################################################################################
@@ -17,6 +18,7 @@ from typing import NamedTuple
 ########################################################################################################################
 
 VERTEX_ADJACENCIES_LINE_PATTERN = re.compile(r'^([a-z]+): *([ a-z]+)$')
+MIN_CUT_STAGE_ONE_FAILURE_PROBABILITY = 0.01
 
 
 class UndirectedGraph(NamedTuple):
@@ -49,8 +51,7 @@ class UndirectedGraph(NamedTuple):
     def calculate_min_cut(self, expected_min_cut_size: int) -> tuple[set[str], set[str]]:
         """
         This is a simplified implementation of Karger's random contraction algorithm for computing the minimum cut of a
-        graph. Rather than running the algorithm the traditional n^2 × ln(n) times (for a failure rate of 1 ÷ n), we'll
-        just keep trying until we hit the expected min cut size.
+        graph.
 
         >>> (first_disjoint_subset, second_disjoint_subset) = UndirectedGraph.from_lines([
         ...     'jqt: rhn xhk nvd',
@@ -72,12 +73,37 @@ class UndirectedGraph(NamedTuple):
         >>> sorted(second_disjoint_subset)
         ['cmg', 'frs', 'lhk', 'lsr', 'nvd', 'pzl', 'qnr', 'rsh', 'rzs']
         """
+        stage_one_min_vertices = ceil(2 / MIN_CUT_STAGE_ONE_FAILURE_PROBABILITY)
+        stage_one_results: Optional[tuple[dict[str, Counter], list[tuple[str, str]], set[tuple[str, str]], dict[str, set[str]]]] = None
+        stage_two_iterations = 1
+        stage_two_max_iterations = ceil(comb(stage_one_min_vertices, 2) * log(stage_one_min_vertices))
         while True:
-            vertex_adjacencies = {vertex: Counter(adjacent_vertices) for (vertex, adjacent_vertices) in self.vertex_adjacencies.items()}
-            edges_list = list(self.edges)
-            edges_set = self.edges.copy()
-            vertex_contractions = {vertex: {vertex} for vertex in self.vertex_adjacencies.keys()}
+            if stage_one_results is not None:
+                vertex_adjacencies = {vertex: vertices.copy() for (vertex, vertices) in stage_one_results[0].items()}
+                edges_list = stage_one_results[1].copy()
+                edges_set = stage_one_results[2].copy()
+                vertex_contractions = {vertex: vertices.copy() for (vertex, vertices) in stage_one_results[3].items()}
+            else:
+                vertex_adjacencies = {vertex: Counter(adjacent_vertices) for (vertex, adjacent_vertices) in self.vertex_adjacencies.items()}
+                edges_list = list(self.edges)
+                edges_set = self.edges.copy()
+                vertex_contractions = {vertex: {vertex} for vertex in self.vertex_adjacencies.keys()}
             while len(edges_list) > 1:
+                # Optimise for large numbers of vertices.
+                if len(vertex_adjacencies) == stage_one_min_vertices:
+                    if stage_one_results is not None:
+                        stage_two_iterations += 1
+                    else:
+                        stage_one_results = (
+                            {vertex: vertices.copy() for (vertex, vertices) in vertex_adjacencies.items()},
+                            edges_list.copy(),
+                            edges_set.copy(),
+                            {vertex: vertices.copy() for (vertex, vertices) in vertex_contractions.items()},
+                        )
+                        stage_two_iterations = 1
+                if stage_two_iterations >= stage_two_max_iterations:
+                    stage_one_results = None
+                # Pick a random edge to contract.
                 (supernode, subnode) = edges_list.pop(randrange(len(edges_list)))
                 edges_set.remove((supernode, subnode))
                 # No self-loops.
