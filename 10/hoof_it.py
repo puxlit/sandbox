6e49283b7ece119d2a7b5a894bf9e4d9c4230562
Xiao Di Guan <xdg@puxlit.net>
2024-12-10 17:36:57 +1100

    Solve second part of day 10

--- 10/hoof_it.py
+++ 10/hoof_it.py
@@ -7,6 +7,7 @@
 
 from collections import deque
 from collections.abc import Iterable, Iterator
+from heapq import heappop, heappush
 from typing import NamedTuple
 
 
@@ -36,7 +37,7 @@ class TopographicMap(NamedTuple):
                 width = len(line)
             elif len(line) != width:
                 raise ValueError(f'Width of line {y + 1} differs from line 1 ({len(line)} ≠ {width})')
-            row = tuple(int(height) for height in line)
+            row = tuple((int(height) if height != '.' else -1) for height in line)
             rows.append(row)
             for (x, height) in enumerate(row):
                 if height == 0:
@@ -62,6 +63,36 @@ class TopographicMap(NamedTuple):
         ...     '9876',
         ... ]).score_trailheads().items())
         [(Coordinate(x=0, y=0), 1)]
+        >>> sorted(TopographicMap.from_lines([
+        ...     '...0...',
+        ...     '...1...',
+        ...     '...2...',
+        ...     '6543456',
+        ...     '7.....7',
+        ...     '8.....8',
+        ...     '9.....9',
+        ... ]).score_trailheads().items())
+        [(Coordinate(x=3, y=0), 2)]
+        >>> sorted(TopographicMap.from_lines([
+        ...     '..90..9',
+        ...     '...1.98',
+        ...     '...2..7',
+        ...     '6543456',
+        ...     '765.987',
+        ...     '876....',
+        ...     '987....',
+        ... ]).score_trailheads().items())
+        [(Coordinate(x=3, y=0), 4)]
+        >>> sorted(TopographicMap.from_lines([
+        ...     '10..9..',
+        ...     '2...8..',
+        ...     '3...7..',
+        ...     '4567654',
+        ...     '...8..3',
+        ...     '...9..2',
+        ...     '.....01',
+        ... ]).score_trailheads().items())
+        [(Coordinate(x=1, y=0), 1), (Coordinate(x=5, y=6), 2)]
         >>> sorted(TopographicMap.from_lines([
         ...     '89010123',
         ...     '78121874',
@@ -92,6 +123,70 @@ class TopographicMap(NamedTuple):
                         to_visit.append(next_position)
             scores[trailhead] = score
         return scores
+
+    def rate_trailheads(self) -> dict[Coordinate, int]:
+        """
+        >>> sorted(TopographicMap.from_lines([
+        ...     '.....0.',
+        ...     '..4321.',
+        ...     '..5..2.',
+        ...     '..6543.',
+        ...     '..7..4.',
+        ...     '..8765.',
+        ...     '..9....',
+        ... ]).rate_trailheads().items())
+        [(Coordinate(x=5, y=0), 3)]
+        >>> sorted(TopographicMap.from_lines([
+        ...     '..90..9',
+        ...     '...1.98',
+        ...     '...2..7',
+        ...     '6543456',
+        ...     '765.987',
+        ...     '876....',
+        ...     '987....',
+        ... ]).rate_trailheads().items())
+        [(Coordinate(x=3, y=0), 13)]
+        >>> sorted(TopographicMap.from_lines([
+        ...     '012345',
+        ...     '123456',
+        ...     '234567',
+        ...     '345678',
+        ...     '4.6789',
+        ...     '56789.',
+        ... ]).rate_trailheads().items())
+        [(Coordinate(x=0, y=0), 227)]
+        >>> sorted(TopographicMap.from_lines([
+        ...     '89010123',
+        ...     '78121874',
+        ...     '87430965',
+        ...     '96549874',
+        ...     '45678903',
+        ...     '32019012',
+        ...     '01329801',
+        ...     '10456732',
+        ... ]).rate_trailheads().items())
+        [(Coordinate(x=0, y=6), 5), (Coordinate(x=1, y=7), 5), (Coordinate(x=2, y=0), 20), (Coordinate(x=2, y=5), 1), (Coordinate(x=4, y=0), 24), (Coordinate(x=4, y=2), 10), (Coordinate(x=5, y=5), 4), (Coordinate(x=6, y=4), 4), (Coordinate(x=6, y=6), 8)]
+        """
+        ratings = {}
+        for trailhead in self.trailheads:
+            rating = 0
+            distinct_trails = {trailhead: 1}
+            to_visit = [(0, trailhead)]
+            while to_visit:
+                (height, position) = heappop(to_visit)
+                distinct_trails_to_position = distinct_trails[position]
+                if height == 9:
+                    rating += distinct_trails_to_position
+                    continue
+                next_height = height + 1
+                for next_position in self.neighbours(position, next_height):
+                    if next_position not in distinct_trails:
+                        distinct_trails[next_position] = distinct_trails_to_position
+                        heappush(to_visit, (next_height, next_position))
+                    else:
+                        distinct_trails[next_position] += distinct_trails_to_position
+            ratings[trailhead] = rating
+        return ratings
 
 
 ########################################################################################################################
@@ -124,6 +219,28 @@ def sum_trailhead_scores(lines: Iterable[str]) -> int:
 
 
 ########################################################################################################################
+# Part 2
+########################################################################################################################
+
+def sum_trailhead_ratings(lines: Iterable[str]) -> int:
+    """
+    >>> sum_trailhead_ratings([
+    ...     '89010123',
+    ...     '78121874',
+    ...     '87430965',
+    ...     '96549874',
+    ...     '45678903',
+    ...     '32019012',
+    ...     '01329801',
+    ...     '10456732',
+    ... ])
+    81
+    """
+    map_ = TopographicMap.from_lines(lines)
+    return sum(map_.rate_trailheads().values())
+
+
+########################################################################################################################
 # CLI bootstrap
 ########################################################################################################################
 
@@ -138,6 +255,8 @@ def main() -> None:
 
     if args.part == 1:
         print(sum_trailhead_scores(lines))
+    elif args.part == 2:
+        print(sum_trailhead_ratings(lines))
     else:
         raise ValueError(f'{args.part} is not a valid part')
 
