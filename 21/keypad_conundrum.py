f5bac0a3877eb62609e4ada6d92e5986cf996ff7
Xiao Di Guan <xdg@puxlit.net>
2024-12-22 00:13:59 +1100

    Solve first part of day 21

--- 21/keypad_conundrum.py
+++ 21/keypad_conundrum.py
@@ -7,7 +7,8 @@
 
 from collections.abc import Iterable, Iterator
 from functools import cache
-from typing import NamedTuple
+from itertools import permutations
+from typing import NamedTuple, Optional
 
 
 ########################################################################################################################
@@ -29,6 +30,7 @@ NUMERIC_KEYPAD_BUTTON_COORDINATES = {
     'A': (0, 2),
 }
 NUMERIC_KEYPAD_COORDINATE_BUTTONS = {coordinate: button for (button, coordinate) in NUMERIC_KEYPAD_BUTTON_COORDINATES.items()}
+
 # (0, 0) is the empty gap, rows count downwards, columns count rightwards.
 DIRECTIONAL_KEYPAD_BUTTON_COORDINATES = {
     '^': (0, 1),
@@ -57,39 +59,80 @@ def parse_codes(lines: Iterable[str]) -> Iterator[Code]:
         yield Code.from_line(line)
 
 
+# Some general observations on "complexity" follow.
+#
+#   - At first blush, to minimise button presses the next layer up, it seems sufficient to consolidate movements along
+#     each axis. (For example, `>^^A` translates to `vA<^AA>A`, whereas `^>^A` translates to the longer `<A>vA<^A>A`.)
+#   - However, between directional keypad layers, the shortest sequence of button presses may produce a longer sequence
+#     of button presses the next layer up compared to a slightly longer sequence of button presses.
+
+
 @cache
-def numeric_to_directional_keypad_button_press(from_button_press: str, to_button_press: str) -> str:
+def numeric_to_directional_keypad_button_press(from_button_press: str, to_button_press: str, depth: int) -> str:
+    assert depth >= 0
     if to_button_press == from_button_press:
+        # At the end of each button press, actors higher up the stack should be poised over the activate button.
         return 'A'
+
     (from_button_row, from_button_column) = NUMERIC_KEYPAD_BUTTON_COORDINATES[from_button_press]
     (to_button_row, to_button_column) = NUMERIC_KEYPAD_BUTTON_COORDINATES[to_button_press]
-    # To minimise button presses the next layer up, we want to consolidate movements along each axis. Prefer moving
-    # horizontally first (except when that'd take us over the empty gap) because that's what the examples prefer.
     horizontal_button_presses = ('>' if (to_button_column > from_button_column) else '<') * abs(to_button_column - from_button_column)
     vertical_button_presses = ('^' if (to_button_row > from_button_row) else 'v') * abs(to_button_row - from_button_row)
-    if (from_button_row == 0) and (to_button_column == 0):
-        return vertical_button_presses + horizontal_button_presses + 'A'
-    else:
-        return horizontal_button_presses + vertical_button_presses + 'A'
+    routes = set(''.join(permutation) for permutation in permutations(horizontal_button_presses + vertical_button_presses))
+    # Remove permutation that takes us over the empty gap.
+    if (from_button_row == to_button_column == 0):
+        routes.remove(horizontal_button_presses + vertical_button_presses)
+    elif (from_button_column == to_button_row == 0):
+        routes.remove(vertical_button_presses + horizontal_button_presses)
+
+    best_translated_button_presses: Optional[str] = None
+    for route in sorted(routes):
+        button_presses = route + 'A'
+        translated_button_presses = button_presses if (depth == 0) else directional_to_directional_keypad_button_presses(button_presses, depth - 1)
+        if (best_translated_button_presses is None) or len(translated_button_presses) < len(best_translated_button_presses):
+            best_translated_button_presses = translated_button_presses
+    assert best_translated_button_presses is not None
+    return best_translated_button_presses
 
 
-def numeric_to_directional_keypad_button_presses(button_presses: str) -> str:
+def numeric_to_directional_keypad_button_presses(button_presses: str, depth: int) -> str:
     """
-    >>> numeric_to_directional_keypad_button_presses('029A')
-    '<A^A>^^AvvvA'
-    >>> numeric_to_directional_keypad_button_presses('980A')
-    '^^^A<AvvvA>A'
-    >>> numeric_to_directional_keypad_button_presses('179A')
-    '^<<A^^A>>AvvvA'
-    >>> numeric_to_directional_keypad_button_presses('456A')
-    '^^<<A>A>AvvA'
-    >>> numeric_to_directional_keypad_button_presses('379A')
-    '^A<<^^A>>AvvvA'
+    >>> len(numeric_to_directional_keypad_button_presses('029A', 0))
+    12
+    >>> len(numeric_to_directional_keypad_button_presses('029A', 1))
+    28
+    >>> len(numeric_to_directional_keypad_button_presses('029A', 2))
+    68
+    >>> len(numeric_to_directional_keypad_button_presses('980A', 0))
+    12
+    >>> len(numeric_to_directional_keypad_button_presses('980A', 1))
+    26
+    >>> len(numeric_to_directional_keypad_button_presses('980A', 2))
+    60
+    >>> len(numeric_to_directional_keypad_button_presses('179A', 0))
+    14
+    >>> len(numeric_to_directional_keypad_button_presses('179A', 1))
+    28
+    >>> len(numeric_to_directional_keypad_button_presses('179A', 2))
+    68
+    >>> len(numeric_to_directional_keypad_button_presses('456A', 0))
+    12
+    >>> len(numeric_to_directional_keypad_button_presses('456A', 1))
+    26
+    >>> len(numeric_to_directional_keypad_button_presses('456A', 2))
+    64
+    >>> len(numeric_to_directional_keypad_button_presses('379A', 0))
+    14
+    >>> len(numeric_to_directional_keypad_button_presses('379A', 1))
+    28
+    >>> len(numeric_to_directional_keypad_button_presses('379A', 2))
+    64
     """
+    assert depth >= 0
     translated_button_presses: list[str] = []
     prev_button_press = 'A'
     for button_press in button_presses:
-        translated_button_presses.append(numeric_to_directional_keypad_button_press(prev_button_press, button_press))
+        translated_button_presses.append(numeric_to_directional_keypad_button_press(prev_button_press, button_press, depth))
         prev_button_press = button_press
     return ''.join(translated_button_presses)
 
@@ -119,53 +162,40 @@ def execute_directional_to_numeric_keypad_button_presses(button_presses: str) ->
 
 
 @cache
-def directional_to_directional_keypad_button_press(from_button_press: str, to_button_press: str) -> str:
+def directional_to_directional_keypad_button_press(from_button_press: str, to_button_press: str, depth: int) -> str:
+    assert depth >= 0
     if to_button_press == from_button_press:
+        # At the end of each button press, actors higher up the stack should be poised over the activate button.
         return 'A'
+
     (from_button_row, from_button_column) = DIRECTIONAL_KEYPAD_BUTTON_COORDINATES[from_button_press]
     (to_button_row, to_button_column) = DIRECTIONAL_KEYPAD_BUTTON_COORDINATES[to_button_press]
-    # To minimise button presses the next layer up, we want to consolidate movements along each axis. Prefer moving
-    # horizontally first (except when that'd take us over the empty gap) because that's what the examples prefer.
     horizontal_button_presses = ('>' if (to_button_column > from_button_column) else '<') * abs(to_button_column - from_button_column)
     vertical_button_presses = ('v' if (to_button_row > from_button_row) else '^') * abs(to_button_row - from_button_row)
-    if (from_button_row == 0) and (to_button_column == 0):
-        return vertical_button_presses + horizontal_button_presses + 'A'
-    else:
-        return horizontal_button_presses + vertical_button_presses + 'A'
+    routes = set(''.join(permutation) for permutation in permutations(horizontal_button_presses + vertical_button_presses))
+    # Remove permutation that takes us over the empty gap.
+    if (from_button_row == to_button_column == 0):
+        routes.remove(horizontal_button_presses + vertical_button_presses)
+    elif (from_button_column == to_button_row == 0):
+        routes.remove(vertical_button_presses + horizontal_button_presses)
+
+    best_translated_button_presses: Optional[str] = None
+    for route in sorted(routes):
+        button_presses = route + 'A'
+        translated_button_presses = button_presses if (depth == 0) else directional_to_directional_keypad_button_presses(button_presses, depth - 1)
+        if (best_translated_button_presses is None) or len(translated_button_presses) < len(best_translated_button_presses):
+            best_translated_button_presses = translated_button_presses
+    assert best_translated_button_presses is not None
+    return best_translated_button_presses
 
 
-def directional_to_directional_keypad_button_presses(button_presses: str) -> str:
-    """
-    >>> directional_to_directional_keypad_button_presses('<A^A>^^AvvvA')  # 029A
-    'v<<A>>^A<A>AvA<^AA>A<vAAA>^A'
-    >>> # directional_to_directional_keypad_button_presses('v<<A>>^A<A>AvA<^AA>A<vAAA>^A')
-    >>> # expected (from example): '<vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A'
-    >>> # what we produce        : '<vA<AA>>^AvAA<^A>Av<<A>>^AvA^A<vA>^Av<<A>^A>AAvA^Av<<A>A>^AAAvA<^A>A'
-    >>> directional_to_directional_keypad_button_presses('^^^A<AvvvA>A')  # 980A
-    '<AAA>Av<<A>>^A<vAAA>^AvA^A'
-    >>> # directional_to_directional_keypad_button_presses('<AAA>Av<<A>>^A<vAAA>^AvA^A')
-    >>> # expected (from example): '<v<A>>^AAAvA^A<vA<AA>>^AvAA<^A>A<v<A>A>^AAAvA<^A>A<vA>^A<A>A'
-    >>> # what we produce        : 'v<<A>>^AAAvA^A<vA<AA>>^AvAA<^A>Av<<A>A>^AAAvA<^A>A<vA>^A<A>A'
-    >>> directional_to_directional_keypad_button_presses('^<<A^^A>>AvvvA')  # 179A
-    '<Av<AA>>^A<AA>AvAA^A<vAAA>^A'
-    >>> # directional_to_directional_keypad_button_presses('<Av<AA>>^A<AA>AvAA^A<vAAA>^A')  # 179A
-    >>> # expected (from example): '<v<A>>^A<vA<A>>^AAvAA<^A>A<v<A>>^AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A'
-    >>> # what we produce        : 'v<<A>>^A<vA<A>>^AAvAA<^A>Av<<A>>^AAvA^A<vA>^AA<A>Av<<A>A>^AAAvA<^A>A'
-    >>> directional_to_directional_keypad_button_presses('^^<<A>A>AvvA')  # 456A
-    '<AAv<AA>>^AvA^AvA^A<vAA>^A'
-    >>> # directional_to_directional_keypad_button_presses('<AAv<AA>>^AvA^AvA^A<vAA>^A')
-    >>> # expected (from example): '<v<A>>^AA<vA<A>>^AAvAA<^A>A<vA>^A<A>A<vA>^A<A>A<v<A>A>^AAvA<^A>A'
-    >>> # what we produce        : 'v<<A>>^AA<vA<A>>^AAvAA<^A>A<vA>^A<A>A<vA>^A<A>Av<<A>A>^AAvA<^A>A'
-    >>> directional_to_directional_keypad_button_presses('^A<<^^A>>AvvvA')  # 379A
-    '<A>Av<<AA>^AA>AvAA^A<vAAA>^A'
-    >>> # directional_to_directional_keypad_button_presses('<A>Av<<AA>^AA>AvAA^A<vAAA>^A')
-    >>> # expected (from example): '<v<A>>^AvA^A<vA<AA>>^AAvA<^A>AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A'
-    >>> # what we produce        : 'v<<A>>^AvA^A<vA<AA>>^AAvA<^A>AAvA^A<vA>^AA<A>Av<<A>A>^AAAvA<^A>A'
-    """
+@cache
+def directional_to_directional_keypad_button_presses(button_presses: str, depth: int) -> str:
+    assert depth >= 0
     translated_button_presses: list[str] = []
     prev_button_press = 'A'
     for button_press in button_presses:
-        translated_button_presses.append(directional_to_directional_keypad_button_press(prev_button_press, button_press))
+        translated_button_presses.append(directional_to_directional_keypad_button_press(prev_button_press, button_press, depth))
         prev_button_press = button_press
     return ''.join(translated_button_presses)
 
@@ -213,10 +243,8 @@ def calculate_code_complexity(code: Code) -> int:
     >>> calculate_code_complexity(Code.from_line('379A'))
     24256
     """
-    first_robot_remote_button_presses = numeric_to_directional_keypad_button_presses(code.button_presses)
-    second_robot_remote_button_presses = directional_to_directional_keypad_button_presses(first_robot_remote_button_presses)
-    third_robot_remote_button_presses = directional_to_directional_keypad_button_presses(second_robot_remote_button_presses)
-    return len(third_robot_remote_button_presses) * code.number
+    button_presses = numeric_to_directional_keypad_button_presses(code.button_presses, 2)
+    return len(button_presses) * code.number
 
 
 def sum_code_complexities(lines: Iterable[str]) -> int:
