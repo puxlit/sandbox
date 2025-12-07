0a8a9bdd423defeefdbc62e69b5c8c752ad67f35
Xiao Di Guan <xdg@puxlit.net>
2025-12-07 22:45:33 +1100

    Solve second part of day 7

--- 7/laboratories.py
+++ 7/laboratories.py
@@ -6,7 +6,7 @@
 ########################################################################################################################
 
 from collections.abc import Iterable
-from functools import cache, reduce
+from functools import reduce
 
 
 ########################################################################################################################
@@ -18,15 +18,15 @@ BEAM_MANIFOLD_ENTRANCE = 'S'
 BEAM_SPLITTER = '^'
 
 
-def parse_diagram(lines: Iterable[str]) -> tuple[tuple[bool, ...], tuple[tuple[bool, ...], ...]]:
+def parse_diagram(lines: Iterable[str]) -> tuple[tuple[int, ...], tuple[tuple[bool, ...], ...]]:
     lines_iter = iter(lines)
 
-    beam_positions = tuple({
-        EMPTY_SPACE: False,
-        BEAM_MANIFOLD_ENTRANCE: True,
+    beam_timelines = tuple({
+        EMPTY_SPACE: 0,
+        BEAM_MANIFOLD_ENTRANCE: 1,
     }[char] for char in next(lines_iter))
-    assert (width := len(beam_positions)) > 1
-    assert sum(has_beam for has_beam in beam_positions) == 1
+    assert (width := len(beam_timelines)) > 1
+    assert sum(has_beam for has_beam in beam_timelines) == 1
 
     all_splitter_positions: list[tuple[bool, ...]] = []
     for line in lines_iter:
@@ -37,37 +37,36 @@ def parse_diagram(lines: Iterable[str]) -> tuple[tuple[bool, ...], tuple[tuple[b
         assert len(splitter_positions) == width
         all_splitter_positions.append(splitter_positions)
 
-    return (beam_positions, tuple(all_splitter_positions))
+    return (beam_timelines, tuple(all_splitter_positions))
 
 
-@cache
-def simulate_step(beam_positions: tuple[bool, ...], splitter_positions: tuple[bool, ...]) -> tuple[tuple[bool, ...], int]:
-    assert len(splitter_positions) == (width := len(beam_positions))
-    next_beam_positions: list[bool] = [False] * width
+def simulate_step(beam_timelines: tuple[int, ...], splitter_positions: tuple[bool, ...]) -> tuple[tuple[int, ...], int]:
+    assert len(splitter_positions) == (width := len(beam_timelines))
+    next_beam_timelines = list(beam_timelines)
     new_beam_splits = 0
     for i in range(width):
-        if not beam_positions[i]:
+        if not beam_timelines[i]:
             # There's no incoming beam.
             continue
         if not splitter_positions[i]:
             # The incoming beam freely travels through empty space.
-            next_beam_positions[i] = True
             continue
         # We're splittin' the beam.
+        next_beam_timelines[i] = 0
         new_beam_splits += 1
         # Maybe split to the left.
-        if (i > 0) and not next_beam_positions[i - 1]:
-            next_beam_positions[i - 1] = True
+        if i > 0:
+            next_beam_timelines[i - 1] += beam_timelines[i]
         # Maybe split to the right.
-        if (i < width - 1) and not next_beam_positions[i + 1]:
-            next_beam_positions[i + 1] = True
-    return (tuple(next_beam_positions), new_beam_splits)
+        if i < (width - 1):
+            next_beam_timelines[i + 1] += beam_timelines[i]
+    return (tuple(next_beam_timelines), new_beam_splits)
 
 
-def simulate_step_with_stats(beam_positions_and_stats: tuple[tuple[bool, ...], int], splitter_positions: tuple[bool, ...]) -> tuple[tuple[bool, ...], int]:
-    (beam_positions, num_beam_splits) = beam_positions_and_stats
-    (next_beam_positions, new_beam_splits) = simulate_step(beam_positions, splitter_positions)
-    return (next_beam_positions, num_beam_splits + new_beam_splits)
+def simulate_step_with_stats(beam_timelines_and_stats: tuple[tuple[int, ...], int], splitter_positions: tuple[bool, ...]) -> tuple[tuple[int, ...], int]:
+    (beam_timelines, num_beam_splits) = beam_timelines_and_stats
+    (next_beam_timelines, new_beam_splits) = simulate_step(beam_timelines, splitter_positions)
+    return (next_beam_timelines, num_beam_splits + new_beam_splits)
 
 
 ########################################################################################################################
@@ -96,9 +95,40 @@ def count_beam_splits(lines: Iterable[str]) -> int:
     ... ])
     21
     """
-    (initial_beam_positions, all_splitter_positions) = parse_diagram(lines)
-    (final_beam_positions, num_beam_splits) = reduce(simulate_step_with_stats, all_splitter_positions, (initial_beam_positions, 0))
+    (initial_beam_timelines, all_splitter_positions) = parse_diagram(lines)
+    (_, num_beam_splits) = reduce(simulate_step_with_stats, all_splitter_positions, (initial_beam_timelines, 0))
     return num_beam_splits
+
+
+########################################################################################################################
+# Part 2
+########################################################################################################################
+
+def count_timelines(lines: Iterable[str]) -> int:
+    """
+    >>> count_timelines([
+    ...     '.......S.......',
+    ...     '...............',
+    ...     '.......^.......',
+    ...     '...............',
+    ...     '......^.^......',
+    ...     '...............',
+    ...     '.....^.^.^.....',
+    ...     '...............',
+    ...     '....^.^...^....',
+    ...     '...............',
+    ...     '...^.^...^.^...',
+    ...     '...............',
+    ...     '..^...^.....^..',
+    ...     '...............',
+    ...     '.^.^.^.^.^...^.',
+    ...     '...............',
+    ... ])
+    40
+    """
+    (initial_beam_timelines, all_splitter_positions) = parse_diagram(lines)
+    (final_beam_timelines, _) = reduce(simulate_step_with_stats, all_splitter_positions, (initial_beam_timelines, 0))
+    return sum(final_beam_timelines)
 
 
 ########################################################################################################################
@@ -116,6 +146,8 @@ def main() -> None:
 
     if args.part == 1:
         print(count_beam_splits(lines))
+    elif args.part == 2:
+        print(count_timelines(lines))
     else:
         raise ValueError(f'{args.part} is not a valid part')
 
